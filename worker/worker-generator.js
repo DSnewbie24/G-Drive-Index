4520c990ea9a66d5c611211a1695dccb14dd2517
DSnewbie24 <87238325+DSnewbie24@users.noreply.github.com>
2021-07-12 18:48:11 +0530

    2.0.21mod

--- worker/worker-generator.js
+++ worker/worker-generator.js
@@ -7,10 +7,10 @@ const clientId = '746239575955-oao9hkv614p8glrqpvuh5i8mqfoq145b.apps.googleuserc
 const clientSecret = 'u5a1CSY5pNjdD2tGTU93TTnI';
 const grantType = 'authorization_code';
 const redirectURI = 'urn:ietf:wg:oauth:2.0:oob';
-const jsURL = 'https://gitlab.com/ParveenBhadooOfficial/Google-Drive-Index/-/raw/master/worker/worker-beta.js';
+const jsURL = 'https://raw.githubusercontent.com/DSnewbie24/G-Drive-Index/master/worker/worker-beta.js';
 
 async function handleRequest(request) {
-    let title = getParameterByName(request.url, 'site_name') || 'Bhadoo Drive Index';
+    let title = getParameterByName(request.url, 'site_name') || 'Placebo Drive Index';
     let auth_code = getParameterByName(request.url, 'auth_code');
     let root = getParameterByName(request.url, 'root') || 'root';
     let user = getParameterByName(request.url, 'user') || '';
@@ -45,7 +45,7 @@ async function handleRequest(request) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
-    <title>Bhadoo Drive Index - Beta</title>
+    <title>Placeboo Drive Index - Beta</title>
     <link rel="icon" type="image/x-icon" href="https://www.cloudflare.com/favicon.ico"/>
     <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.0.0/dist/slate/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
@@ -284,7 +284,7 @@ async function handleRequest(request) {
 <body">
 <div class="container">
     <br><br>
-    <a href="https://www.npmjs.com/package/@googledrive/index"><h3>Bhadoo Index Generator - Beta</h3></a>
+    <a href="https://www.npmjs.com/package/@googledrive/index"><h3>Placeboo Index Generator - Beta</h3></a>
     <br>
         <div>
             <h3>1. <a
